5c0dee912dda5ef9e05dc59db76845622ce5af13
reidlo5135 <201840103@email.daelim.ac.kr>
2023-02-01 17:17:15 +0900

    refactor : add modules

--- client/public/electron.js
+++ client/public/electron.js
@@ -8,8 +8,8 @@ var mainWindow;
 remoteMain.initialize();
 var createWindow = function () {
     mainWindow = new electron_1.BrowserWindow({
-        width: 900,
-        height: 680,
+        width: 1680,
+        height: 840,
         center: true,
         kiosk: !isDev,
         resizable: true,
@@ -23,8 +23,6 @@ var createWindow = function () {
             webSecurity: false
         }
     });
-    // production에서는 패키지 내부 리소스에 접근.
-    // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드.
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : "file://".concat(path.join(__dirname, '../build/index.html')));
     if (isDev) {
         mainWindow.webContents.openDevTools({ mode: 'detach' });
@@ -34,11 +32,7 @@ var createWindow = function () {
     mainWindow.on('closed', function () { return (mainWindow = undefined); });
     mainWindow.focus();
 };
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 electron_1.app.on('ready', createWindow);
-// Quit when all windows are closed.
 electron_1.app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         electron_1.app.quit();
