1b691d084050b61973b9c6defd14d9c42b0cee94
reidlo5135 <201840103@email.daelim.ac.kr>
2023-02-03 12:53:25 +0900

    feat : add User sequelize model

--- server/src/api/user/local/UserController.ts
+++ server/src/api/user/local/UserController.ts
@@ -1,15 +1,19 @@
 import { Request, Response } from "express";
-const service = require('../../../service/user/local/UserService');
+import * as service from "../../../service/user/local/UserService";
 
-const login = async(req: Request, res: Response) => {
-    console.log('UserController login');
+export function login(req: Request, res: Response) {
     const params = {
         email : req.body.email,
         password : req.body.password
     };
-    console.log('email : ', params.email);
-}
-
-export = {
-    login
+    console.log('UserController login email : ', JSON.stringify(params));
+    service.generateToken(params)
+        .then(
+            (resolve:any) => {
+                console.log("UserController login resolved : ", JSON.stringify(resolve));
+            },
+            (reject:any) => {
+                console.log("UserController login rejected : ", JSON.stringify(reject));
+            }
+        )
 }
--- server/src/api/user/local/UserRouter.ts
+++ server/src/api/user/local/UserRouter.ts
@@ -1,7 +1,34 @@
 import express from "express";
+import * as controller from "../local/UserController";
 const router = express.Router();
-const controller = require("../local/UserController");
 
+/**
+ * @swagger
+ * paths:
+ *  /api/user/users:
+ *    get:
+ *      summary: "유저 데이터 전체조회"
+ *      description: "서버에 데이터를 보내지 않고 Get방식으로 요청"
+ *      tags: [Users]
+ *      responses:
+ *        "200":
+ *          description: 전체 유저 정보
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    ok:
+ *                      type: boolean
+ *                    users:
+ *                      type: object
+ *                      example:
+ *                          [
+ *                            { "id": 1, "name": "유저1" },
+ *                            { "id": 2, "name": "유저2" },
+ *                            { "id": 3, "name": "유저3" },
+ *                          ]
+ */
 router.post('/login', controller.login);
 
 export = router;
