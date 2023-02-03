66cbcb179a6e0b9672626320877fee44ca2bc34e
reidlo5135 <201840103@email.daelim.ac.kr>
2023-02-03 12:53:04 +0900

    feat : add swagger modules

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -5,6 +5,11 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import UserRouter from "../api/user/local/UserRouter";
 import SocialUserRouter from "../api/user/social/SocialUserRouter";
+import {swaggerOptions} from "./swagger";
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const specs = swaggerJsdoc(swaggerOptions);
 
 export default async ({ app }: { app: express.Application }) => {
     app.use(logger("dev"));
@@ -14,6 +19,11 @@ export default async ({ app }: { app: express.Application }) => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(cors({ origin: "http://localhost:3000/" }));
+    app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(specs, {explorer: true})
+    );
     app.use("/v1/api/user/", UserRouter);
     app.use("/v1/api/social/", SocialUserRouter);
 
